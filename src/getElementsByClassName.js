8beba8acbd071db98bb39a22588f25bfec331749
Sean <sean.jung23@gmail.com>
2023-12-05 15:57:29 -0500

    function complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,33 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className, element) {
-  // your code here
-
-  // I - class name, elements of the HTML body
-  // O - Array without nesting (flattened)
-  // C - Have to use document.body, element.childNodes, and element.classList
-  // E - None for now
-
-  // Set variables for the HTML elements
-  // Make a result variable for our flattened array
-  // Set base case to stop when there are no child nodes
-  //   Return any html elements that have the classname or return nothing
-  // Set recursive function to iterate over each branch of HTML elements
-  //   Concat any html elements that have the classname to the result variable
-  // Return flattened array
-
-  var element = element === undefined ? document.body : element;
-  var children = element.childNodes;
+var getElementsByClassName = function (className, node) {
+  var node = node || document.body;
   var result = [];
 
-  if (element.classList && element.classList.contains(className)) {
-    result.push(element);
+  if (node.classList && node.classList.contains(className)) {
+    result.push(node);
   }
-  if (children) {
-    children.forEach(function(item) {
-      result = result.concat(getElementsByClassName(className, item));
-    });
+
+  for (var i = 0; i < node.childNodes.length; i++) {
+    result = result.concat(
+      getElementsByClassName(className, node.childNodes[i])
+    );
   }
 
   return result;
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,30 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    var stringifiedArray = obj.map(function (val) {
+      return stringifyJSON(val);
+    });
+
+    return '[' + stringifiedArray.join(',') + ']';
+  } else if (typeof obj === 'object' && obj !== null) {
+    var objectKeys = Object.keys(obj);
+
+    var stringifiedObject = objectKeys
+      .filter(function (key) {
+        return obj[key] !== undefined && typeof obj[key] !== 'function';
+      })
+      .map(function (key) {
+        return stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      });
+
+    return '{' + stringifiedObject.join(',') + '}';
+  } else {
+    return 'null';
+  }
 };
